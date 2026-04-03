d76afb7248f8c3c0fd3528e2b9e86cbe453616fe
Tylerrocks123 <taalmeida@email.neit.edu>
2026-04-03 12:29:54 -0400

    testinginhnihshs

--- js/GameLoop.js
+++ js/GameLoop.js
@@ -1,9 +1,25 @@
 var canvas;
 var context;
 var player;
+var timer;
+var interval = 1000/60;
 
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
 
 player = new Player();
-player.draw();
+player.vx = 2;
+player.vy = 0;
+
+timer = setInterval(animate, interval);
+
+function animate()
+{
+    context.clearRect(0,0,canvas.width,canvas.height);
+    player.move();
+    if (player.x > canvas.width + player.width/2)
+    {
+        player.x = -player.width/2
+    }
+    player.draw();
+}
--- js/Player.js
+++ js/Player.js
@@ -11,6 +11,10 @@ function Player()
     //set up player color
     this.color = "#ff0000"
 
+    //set up player's velocity
+    this.vx = 0;
+    this.vy = 0;
+
     this.draw = function()
     {
         context.save();
@@ -18,6 +22,12 @@ function Player()
         context.translate(this.x,this.y);
         context.fillRect((-this.width/2),(-this.height/2),this.width,this.height)
         context.restore();
+    }
+
+    this.move = function()
+    {
+        this.x += this.vx;
+        this.y += this.vy;
     }
 
 }
